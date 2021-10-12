907a7f84c8656a434416946767f160d2fef787ad
peterson <jooxpeters@gmail.com>
2021-10-12 01:57:59 -0300

    fuck this i'm tired

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -28,6 +29,7 @@
 #define light_border    ACS_VLINE, ACS_VLINE, ACS_BOARD, ACS_BOARD, ACS_BOARD, ACS_BOARD, ACS_VLINE, ACS_VLINE 
 #define bold_border     ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK, ACS_BLOCK 
 
+// struct for storing window pos and size
 typedef struct{
     int h;
     int w;
@@ -35,6 +37,7 @@ typedef struct{
     int x;
 }win_size_t;
 
+// struct for storing a frame charset, all sides and corners
 typedef struct{
     chtype ls;
     chtype rs;
@@ -46,11 +49,36 @@ typedef struct{
     chtype br;
 }frame_charset_t;
 
+// type of word break
+typedef enum{
+    strfmt_none                             = 0,  
+
+    strfmt_linebreak_bitmask                = 0x000F,
+    strfmt_linebreak_no_wrap                = (1<<0),  
+    strfmt_linebreak_no_wrap_dot_dot_dot    = (2<<0),              
+    strfmt_linebreak_wrap_cut               = (3<<0),      
+    strfmt_linebreak_wrap_cut_hyphen        = (4<<0),              
+    strfmt_linebreak_wrap_on_word           = (5<<0),  
+
+    strfmt_lines_bitmask                    = 0x00F0,
+    strfmt_lines_scroll_up                  = (1<<4),
+    strfmt_lines_scroll_down                = (2<<4),
+    strfmt_lines_scroll_cut                 = (3<<4),
+    strfmt_lines_scroll_cut_dot_dot_dot     = (4<<4),
+
+    strfmt_align_bitmask                    = 0x0F00,
+    strfmt_align_left                       = (1<<8),
+    strfmt_align_right                      = (2<<8),
+    strfmt_align_center                     = (3<<8) 
+}strfmt_enum;
+
+// set the size of a window
 void wsetsize(WINDOW *window, win_size_t winsize){
     mvwin(window, winsize.y, winsize.x);
     wresize(window, winsize.h, winsize.w);
 }
 
+// draw a rectangle with a frame
 void wdraw_rect(WINDOW *win, int h, int w, int y, int x, frame_charset_t frame, chtype fill){
     for(int i = x; i < x+w; i++){
         for(int j = y; j < y+h; j++){
@@ -86,6 +114,7 @@ void wdraw_rect(WINDOW *win, int h, int w, int y, int x, frame_charset_t frame, 
     }
 }
 
+// draw a border with a frame
 void wborder_frame(WINDOW *win, frame_charset_t frame){
     wborder(win, 
         frame.ls,
@@ -99,10 +128,228 @@ void wborder_frame(WINDOW *win, frame_charset_t frame){
     );
 }
 
+// count text lines and get max width
+void strlnmaxw(char *string, int *lines, int *max_w){
+    if(string == NULL || lines == NULL || max_w == NULL)
+        return;
+
+    *lines = 1;
+    *max_w = 0;
+    int last_max_w = 0;
+    size_t i = 0;
+    
+    while(string[i] != '\0'){
+        (*max_w)++;
+        
+        if(string[i] == '\n'){
+            if(*max_w > last_max_w)
+                last_max_w = *max_w;
+                
+            (*lines)++;
+        }
+
+        i++;
+    }
+
+    // when string is a single line the last_max_w is zero
+    if(last_max_w != 0)
+        *max_w = last_max_w;
+}
+
+// run all whitespace chars
+void run_whitespace(char **stream){
+    while( ((**stream >= '\b' && **stream <= '\r') || **stream == 32) )
+        (*stream)++;
+}
+
+// insert substring on a string at start location. The base string will be reallocated if necessary, thus we pass a double pointer.
+// The return value points to the string right after the inserted substring.
+char *strins_substr(char **string_base_address, char *start, char *substr){
+    if(*string_base_address == NULL || (start == NULL || start < string_base_address) || substr == NULL)
+        return NULL;
+
+    size_t newstr_len = strlen(*string_base_address) + strlen(substr) + 1;
+    char *newstr = (char*)calloc(newstr_len, sizeof(char));
+
+
+    if(start == *string_base_address){
+        snprintf(newstr, newstr_len, "%s%s", substr, *string_base_address);
+        start = (char*)(newstr + strlen(substr));
+    }
+    else if(*start == '\0'){
+        snprintf(newstr, newstr_len, "%s%s", *string_base_address, substr);
+        start = (char*)(newstr + strlen(substr) + strlen(*string_base_address));
+    }
+    else{
+        size_t offset = start - (*string_base_address);
+        char intersection = *start;
+        *start = '\0';
+        start++;
+
+        snprintf(newstr, newstr_len, "%s%c%s%s", *string_base_address, intersection, substr, start);
+
+        start = (char*)(newstr + offset + strlen(substr));
+    }
+
+
+    free(*string_base_address);
+    *string_base_address = newstr;
+
+    return start;
+}
+
+// make a string repeating a char
+char *makechrspan(char chr, size_t len){
+    char *span = (char*)calloc(len + 1, sizeof(char));
+
+    for(int i = 0; i < len; i++)
+        span[i] = chr;
+
+    span[len] = '\0'; 
+
+    return span;
+}
+
+void strfmtln(char **string_base_address, char *line, int minlen, int maxlen, strfmt_enum fmt){
+    if(*string_base_address == NULL || (line == NULL || line < (*string_base_address)))
+        return NULL;
+
+    size_t offset = line - *string_base_address;
+    size_t line_len = strcspn(line, "\n") - 1;
+    char *linebreak = strpbrk(line, "\n");
+    linebreak = '\0';
+
+    // stuff
+    if(line_len < minlen){
+        switch(fmt & strfmt_align_bitmask){
+            case strfmt_align_center:
+                break;
+
+            case strfmt_align_right:
+                break;
+                
+            default:
+            case strfmt_align_left:
+                {
+                    char *spaces = makechrspan(' ', minlen - line_len);
+                    strins_substr(&string_base_address, line, )
+                    free(spaces);
+                }
+                break;
+        }
+    }
+}
+
+// string formatter, non destructive, returns new memory
+char *strfmtr(char *string, int min_lines, int max_lines, int min_line_len, int max_line_len, strfmt_enum fmt){
+    if(string == NULL)
+        return NULL;
+
+    if(min_line_len <= 0 || min_lines <=0 || max_lines < min_lines || max_line_len < min_line_len)
+        return NULL;
+    
+    char *base_string = calloc(1, strlen(string) + 1);
+    if(base_string == NULL)
+        return NULL;
+
+    strcpy(base_string, string);
+
+    char *cur_line = base_string;
+    size_t chars = 0;
+    size_t lines = 0;
+
+    for(char *cursor = base_string; true; cursor++){
+        bool last_loop = false;
+        
+        if(*cursor == '\0')
+            last_loop = true;
+
+        // handle line
+        if(*cursor == '\n' || *cursor == '\0'){
+            if(chars < min_line_len){
+                // stuff with spaces
+            }
+
+            lines++;
+        }
+
+        if(chars > max_line_len){
+            // insert line break
+        }
+
+        if(lines > max_lines){
+            // do line stuff
+        }
+
+        if(last_loop && lines < min_lines){
+            // stuff with line breaks
+        }
+
+        chars++;
+
+        if(last_loop)
+            break;
+    }
+
+    return base_string;
+}
+
+// better mvwprintw
+void mvwprintwln(WINDOW *win, int y, int x, char *string){
+    if(win == NULL || string == NULL)
+        return;
+    
+    char *base_string = calloc(1, strlen(string) + 1);
+    if(base_string == NULL)
+        return;
+
+    strcpy(base_string, string);
+
+    char *cur_string = base_string;
+    int cur_line = 0;
+
+    for(char *cursor = base_string; true; cursor++){
+        if(*cursor == '\n'){
+            *cursor = '\0';
+
+            mvwprintw(win, y + cur_line, x, cur_string);
+
+            cursor++;
+            run_whitespace(&cursor);
+            cur_string = cursor;
+
+            // so the next cursor++ on the for loop goes on top of the next letter
+            cursor--;
+            cur_line++;
+        }
+        else if(*cursor == '\0'){
+            mvwprintw(win, y + cur_line, x, cur_string);
+            break;
+        }
+    }
+
+    free(base_string);
+}
+
+// draw text with a frame
+void wdraw_label(WINDOW *win, int y, int x, int minh, int minw, int maxh, int maxw, 
+    char *text, frame_charset_t frame, chtype fill){
+
+    int lines, max_w;
+    
+    strlnmaxw(text, &lines, &max_w);
+
+
+    wdraw_rect(win, lines + 2, max_w + 2, y, x, frame, fill);
+    
+    mvwprintwln(win, y + 1, x + 1, text);
+}
+
 int main(int argc, char **argv){
 
     initscr();
-    cbreak();
+    // cbreak();
+    timeout(-1);
     noecho();
     clear();
     curs_set(0);
@@ -140,11 +387,13 @@ int main(int argc, char **argv){
     while(1){
         
         // ----------------------- draw ------------------------
+        int height, width;
+        getmaxyx(stdscr, height, width);
         clear();
 
         // main window
         wclear(main_win);
-        wresize(main_win, LINES, COLS);
+        wresize(main_win, height, width);
         wborder_frame(main_win, frame_normal);
         mvwprintw(main_win, 0, 3, "[Talkon - TCP LAN Messenger v1.0]");
 
@@ -152,7 +401,7 @@ int main(int argc, char **argv){
         wclear(nav_win);
         win_size_t nav_win_size = {
             .h = 3, 
-            .w = COLS - 2, 
+            .w = width - 2, 
             .y = 1, 
             .x = 1
         };
@@ -165,8 +414,8 @@ int main(int argc, char **argv){
         // nodes / network
         wclear(nodes_win);
         win_size_t nodes_win_size = {
-            .h = LINES - 5, 
-            .w = COLS * 0.4, 
+            .h = height - 5, 
+            .w = width * 0.4, 
             .y = 4, 
             .x = 1
         };
@@ -174,14 +423,16 @@ int main(int argc, char **argv){
         wsetsize(nodes_win, nodes_win_size);
         wborder_frame(nodes_win, frame_normal);
         mvwprintw(nodes_win, 0, 3, " Network ");
-        wdraw_rect(nodes_win, 7, nodes_win_size.w - 2, 1, 1, frame_dotted, ' ');
+        // wdraw_rect(nodes_win, 7, nodes_win_size.w - 2, 1, 1, frame_dotted, ' ');
+        // wdraw_label(nodes_win, 1, 1, "IP: 0000\nPORT:2222", frame_solid, ' ');
+        mvwprintwln(nodes_win, 1, 1, "IP: 0000\nPORT:2222\nMASK:127");
 
         // input
         wclear(input_win);
         win_size_t input_win_size = {
             .h = 7, 
-            .w = COLS - 2 - nodes_win_size.w, 
-            .y = LINES - 7 - 1, 
+            .w = width - 2 - nodes_win_size.w, 
+            .y = height - 7 - 1, 
             .x = nodes_win_size.w + 1
         };
 
@@ -193,7 +444,7 @@ int main(int argc, char **argv){
         // talk / chat
         wclear(talk_win);
         win_size_t talk_win_size = {
-            .h = LINES - 5 - input_win_size.h, 
+            .h = height - 5 - input_win_size.h, 
             .w = input_win_size.w, 
             .y = 4, 
             .x = nodes_win_size.w + 1
@@ -204,11 +455,11 @@ int main(int argc, char **argv){
         mvwprintw(talk_win, 0, 3, " Chat ");
 
         // render
-        wrefresh(nav_win);
-        wrefresh(main_win);
-        wrefresh(nodes_win);
-        wrefresh(talk_win);
-        wrefresh(input_win);
+        // wrefresh(nav_win);
+        // wrefresh(main_win);
+        // wrefresh(nodes_win);
+        // wrefresh(talk_win);
+        // wrefresh(input_win);
         update_panels();
         refresh();
 
@@ -220,6 +471,9 @@ int main(int argc, char **argv){
                 case 'q':
                     endwin();
                     exit(0);
+                    break;
+
+                default:
                     break;
             }
 
