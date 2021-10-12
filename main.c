407e80aa5d27ad841897413d271f25f7a4a5945f
peterson <jooxpeters@gmail.com>
2021-10-12 19:58:57 -0300

    string formatting utilities ok, label func ok

--- main.c
+++ main.c
@@ -56,21 +56,20 @@ typedef enum{
     strfmt_linebreak_bitmask                = 0x000F,
     strfmt_linebreak_no_wrap                = (1<<0),  
     strfmt_linebreak_no_wrap_dot_dot_dot    = (2<<0),              
-    strfmt_linebreak_wrap_cut               = (3<<0),      
-    strfmt_linebreak_wrap_cut_hyphen        = (4<<0),              
-    strfmt_linebreak_wrap_on_word           = (5<<0),  
+    strfmt_linebreak_no_wrap_end_str        = (3<<0),              
+    strfmt_linebreak_wrap_cut               = (4<<0),      
+    strfmt_linebreak_wrap_cut_hyphen        = (5<<0),              
+    strfmt_linebreak_wrap_on_word           = (6<<0),  
 
     strfmt_lines_bitmask                    = 0x00F0,
-    strfmt_lines_scroll_up                  = (1<<4),
-    strfmt_lines_scroll_down                = (2<<4),
-    strfmt_lines_scroll_cut                 = (3<<4),
-    strfmt_lines_scroll_cut_dot_dot_dot     = (4<<4),
+    strfmt_lines_scroll                     = (1<<4),
+    strfmt_lines_cut                        = (2<<4),
 
     strfmt_align_bitmask                    = 0x0F00,
     strfmt_align_left                       = (1<<8),
     strfmt_align_right                      = (2<<8),
     strfmt_align_center                     = (3<<8) 
-}strfmt_enum;
+}strfmt_t;
 
 // set the size of a window
 void wsetsize(WINDOW *window, win_size_t winsize){
@@ -129,7 +128,7 @@ void wborder_frame(WINDOW *win, frame_charset_t frame){
 }
 
 // count text lines and get max width
-void strlnmaxw(char *string, int *lines, int *max_w){
+void strlnmaxw(char *string, size_t *lines, size_t *max_w){
     if(string == NULL || lines == NULL || max_w == NULL)
         return;
 
@@ -142,8 +141,11 @@ void strlnmaxw(char *string, int *lines, int *max_w){
         (*max_w)++;
         
         if(string[i] == '\n'){
-            if(*max_w > last_max_w)
+            (*max_w)--;
+            if(*max_w > last_max_w){
                 last_max_w = *max_w;
+                *max_w = 0;
+            }
                 
             (*lines)++;
         }
@@ -162,40 +164,110 @@ void run_whitespace(char **stream){
         (*stream)++;
 }
 
-// insert substring on a string at start location. The base string will be reallocated if necessary, thus we pass a double pointer.
-// The return value points to the string right after the inserted substring.
-char *strins_substr(char **string_base_address, char *start, char *substr){
-    if(*string_base_address == NULL || (start == NULL || start < string_base_address) || substr == NULL)
+// run whitespace backwards
+void run_whitespace_back(char **stream){
+    while( ((**stream >= '\b' && **stream <= '\r') || **stream == 32) )
+        (*stream)--;
+}
+
+// insert substring on a string at start location.
+char *strins_substr(char *string, char *start, char *substr){
+    if(string == NULL || (start == NULL || start < string) || substr == NULL)
         return NULL;
 
-    size_t newstr_len = strlen(*string_base_address) + strlen(substr) + 1;
+    size_t newstr_len = strlen(string) + strlen(substr) + 1;
     char *newstr = (char*)calloc(newstr_len, sizeof(char));
 
 
-    if(start == *string_base_address){
-        snprintf(newstr, newstr_len, "%s%s", substr, *string_base_address);
-        start = (char*)(newstr + strlen(substr));
+    if(start == string){
+        snprintf(newstr, newstr_len, "%s%s", substr, string);
     }
     else if(*start == '\0'){
-        snprintf(newstr, newstr_len, "%s%s", *string_base_address, substr);
-        start = (char*)(newstr + strlen(substr) + strlen(*string_base_address));
+        snprintf(newstr, newstr_len, "%s%s", string, substr);
     }
     else{
-        size_t offset = start - (*string_base_address);
         char intersection = *start;
         *start = '\0';
         start++;
 
-        snprintf(newstr, newstr_len, "%s%c%s%s", *string_base_address, intersection, substr, start);
+        snprintf(newstr, newstr_len, "%s%c%s%s", string, intersection, substr, start);
 
-        start = (char*)(newstr + offset + strlen(substr));
+        start--;
+        *start = intersection;
     }
 
+    return newstr;
+}
 
-    free(*string_base_address);
-    *string_base_address = newstr;
+// concatenates and reallocate accordingly to the expected size
+void strcat_realloc(char **dest, char *string){
+    if(dest == NULL || *dest == NULL || string == NULL)
+        return;
+    
+    size_t newlen = strlen(*dest) + strlen(string);
 
-    return start;
+    char *newstr = (char*)calloc(newlen + 1, sizeof(char));
+
+    strcat(newstr, *dest);
+    strcat(newstr, string);
+
+    free(*dest);
+    *dest = newstr;
+}
+
+// trim string at the end
+char *strtrimend(char *string, char *pbrk){
+    if(string == NULL || (pbrk == NULL || pbrk < string) || *pbrk == '\0' || pbrk == string)
+        return NULL;
+
+    char *string_copy = (char*)calloc(strlen(string) + 1, sizeof(char));
+    strcpy(string_copy, string); 
+    pbrk = (char*)(pbrk - string + string_copy);
+
+    size_t newstr_len = strlen(string_copy) - strlen(pbrk);
+    char *newstr = (char*)calloc(newstr_len + 1, sizeof(char));
+
+    char c = *pbrk; 
+    *pbrk = '\0';
+    strncpy(newstr, string_copy, newstr_len);
+    *pbrk = c;
+
+    free(string_copy);
+    return newstr;
+}
+
+// trim a string from the start
+char *strtrimbeg(char *string, char *pbrk){
+    if(string == NULL || (pbrk == NULL || pbrk < string))
+        return NULL;
+
+    if(*pbrk == '\0'){
+        char *newstr = (char*)calloc(1, sizeof(char));
+        return newstr;
+    }
+    else if(pbrk = string){
+        char *newstr = (char*)calloc(strlen(string) + 1, sizeof(char));
+        strcpy(newstr, string);
+        return newstr;
+    }
+    else{
+        size_t newstr_len = strlen(string) - (size_t)(pbrk - string);
+        char *newstr = (char*)calloc(newstr_len + 1, sizeof(char));
+
+        strncpy(newstr, pbrk, newstr_len);
+
+        return newstr;
+    }
+}
+
+// trim a string from the start, reallocating the passed string
+void strtrimbeg_realloc(char **string, char *pbrk){
+    if(*string == NULL || (pbrk == NULL || pbrk < (*string)))
+        return;
+        
+    char *newstr = strtrimbeg(*string, pbrk);
+    free(*string);
+    *string = newstr;
 }
 
 // make a string repeating a char
@@ -210,38 +282,168 @@ char *makechrspan(char chr, size_t len){
     return span;
 }
 
-void strfmtln(char **string_base_address, char *line, int minlen, int maxlen, strfmt_enum fmt){
-    if(*string_base_address == NULL || (line == NULL || line < (*string_base_address)))
+// align string based on strfmt_align enum options
+char *stralign(char *string, size_t minlen, strfmt_t fmt){
+
+    size_t string_len = strlen(string);
+
+    if(string_len > minlen)
+        minlen = string_len;
+
+    switch(fmt & strfmt_align_bitmask){
+        case strfmt_align_center:
+            {
+                size_t left_padding = (minlen - string_len) / 2;
+                size_t right_padding  = minlen - string_len - left_padding;
+
+                char *spaces_left  = makechrspan(' ', left_padding);
+                char *spaces_right = makechrspan(' ', right_padding);
+                
+                char *padleft = strins_substr(string, string, spaces_left);
+                char *aligned = strins_substr(padleft, strchr(padleft, '\0'), spaces_right);
+
+                free(padleft);
+                free(spaces_left);
+                free(spaces_right);
+
+                return aligned;
+            }
+            break;
+
+        case strfmt_align_right:
+            {
+                char *spaces = makechrspan(' ', minlen - string_len);
+                char *aligned = strins_substr(string, string, spaces);
+                
+                free(spaces);
+
+                return aligned;
+            }
+            break;
+            
+        default:
+        case strfmt_align_left:
+            {
+                char *spaces = makechrspan(' ', minlen - string_len);
+                char *aligned = strins_substr(string, strchr(string, '\0'), spaces);
+
+                free(spaces);
+
+                return aligned;
+            }
+            break;
+    }
+}
+
+// format a single line, null terminated with no line breaks
+char *strfmtln(char *line, size_t minlen, size_t maxlen, strfmt_t fmt, void *optional_data, char **line_break_pos){
+    if(line == NULL || minlen > maxlen)
         return NULL;
 
-    size_t offset = line - *string_base_address;
-    size_t line_len = strcspn(line, "\n") - 1;
-    char *linebreak = strpbrk(line, "\n");
-    linebreak = '\0';
+    size_t line_len = strlen(line);
+    *line_break_pos = NULL;
 
-    // stuff
+    // stuff with whitespace
     if(line_len < minlen){
-        switch(fmt & strfmt_align_bitmask){
-            case strfmt_align_center:
-                break;
-
-            case strfmt_align_right:
-                break;
-                
-            default:
-            case strfmt_align_left:
+        char *aligned = stralign(line, minlen, fmt);
+        return aligned;
+    }
+    // add line break
+    else if(line_len > maxlen){
+        switch(fmt & strfmt_linebreak_bitmask){
+            case strfmt_linebreak_no_wrap:
+            case strfmt_linebreak_wrap_cut:
                 {
-                    char *spaces = makechrspan(' ', minlen - line_len);
-                    strins_substr(&string_base_address, line, )
-                    free(spaces);
+                    char *trimmed = strtrimend(line, (char*)(line + maxlen));
+                    return trimmed;
                 }
                 break;
+
+            case strfmt_linebreak_no_wrap_dot_dot_dot:
+            case strfmt_linebreak_wrap_cut_hyphen:
+            case strfmt_linebreak_no_wrap_end_str:
+                {
+                    char *end;
+                    size_t end_len;
+
+                    switch(fmt & strfmt_linebreak_bitmask){
+                        case strfmt_linebreak_no_wrap_dot_dot_dot:
+                            end = "...";
+                            break;
+                            
+                        case strfmt_linebreak_wrap_cut_hyphen:
+                            end = "-";
+                            break;
+
+                        case strfmt_linebreak_no_wrap_end_str:
+
+                            if(optional_data != NULL)
+                                end = (char*)optional_data;
+                            else
+                                end = "...";
+                                
+                            break;
+                    }
+
+                    end_len = strlen(end);
+                    
+                    char *trimmed = strtrimend(line, (char*)(line + maxlen - end_len));
+
+                    char *cutted = (char*)calloc(maxlen + 1, sizeof(char));
+
+                    snprintf(cutted, maxlen + 1, "%s%s", trimmed, end);
+
+                    free(trimmed);
+                    return cutted;
+                }
+                break;
+
+            case strfmt_linebreak_wrap_on_word:
+                    char *line_brk = (char*)(line + line_len);
+
+                    // go back until whitespace
+                    if(*line_brk != ' '){
+                        for(line_brk = line_brk; line_brk > line; line_brk--){
+                            if(*line_brk == ' ')
+                                break;
+                        }
+                    }
+
+                    // can't linebreak on word because there is only one word
+                    if(line_brk == line)
+                        return strfmtln(line, minlen, maxlen, (fmt & ~(strfmt_linebreak_wrap_on_word)) | strfmt_linebreak_wrap_cut_hyphen, optional_data, line_break_pos);
+
+                    *line_brk = '\0';
+                    // points to breaked line
+                    line_brk++;
+
+                    if(line_break_pos != NULL)
+                        *line_break_pos = line_brk;
+
+                    char *newline_1 = stralign(line, minlen, fmt);
+                    size_t newline_len = strlen(newline_1) + strlen(line_brk);
+                    char *newline = (char*)calloc(newline_len + 1, sizeof(char));
+
+                    snprintf(newline, newline_len, "%s\n%s", newline_1, line_brk);
+
+                    line_brk--;
+                    *line_brk = ' ';
+
+                    free(newline_1);
+
+                    return newline;
+                break;
         }
+    }
+    else{
+        char *newstr = (char*)calloc(strlen(line) + 1, sizeof(char));
+        strcpy(newstr, line);
+        return newstr;
     }
 }
 
 // string formatter, non destructive, returns new memory
-char *strfmtr(char *string, int min_lines, int max_lines, int min_line_len, int max_line_len, strfmt_enum fmt){
+char *strfmtr(char *string, size_t min_lines, size_t max_lines, size_t min_line_len, size_t max_line_len, strfmt_t fmt, void *optional_data){
     if(string == NULL)
         return NULL;
 
@@ -254,44 +456,92 @@ char *strfmtr(char *string, int min_lines, int max_lines, int min_line_len, int 
 
     strcpy(base_string, string);
 
-    char *cur_line = base_string;
-    size_t chars = 0;
+    char *str_fmtd = (char*)calloc(1, sizeof(char));
     size_t lines = 0;
 
-    for(char *cursor = base_string; true; cursor++){
-        bool last_loop = false;
-        
-        if(*cursor == '\0')
-            last_loop = true;
+    // loop throught lines
+    for(char *line = base_string; *line != '\0'; line++){
 
-        // handle line
-        if(*cursor == '\n' || *cursor == '\0'){
-            if(chars < min_line_len){
-                // stuff with spaces
-            }
-
-            lines++;
+        char *linebreak = strchr(line, '\n');
+        char c;
+        if(linebreak != NULL){
+            c = *linebreak;
+            *linebreak = '\0';
+        }
+        else{
+            linebreak = strchr(line, '\0');
+            c = *linebreak;
         }
 
-        if(chars > max_line_len){
-            // insert line break
+        char *line_break_pos;
+
+        //fmt line
+        char *line_fmtd = strfmtln(line, min_line_len, max_line_len, fmt, optional_data, &line_break_pos); 
+
+        *linebreak = c;
+
+        // if not on first line
+        if(lines != 0)
+            strcat_realloc(&str_fmtd, "\n");
+
+        // check if string was line breaked
+        if(line_break_pos != NULL){
+            char *linebreak_on_fmtd = strchr(line_fmtd, '\n');
+            linebreak_on_fmtd = '\0';
+            strcat_realloc(&str_fmtd, line_fmtd);
+        }
+        else{
+            strcat_realloc(&str_fmtd, line_fmtd);
         }
 
-        if(lines > max_lines){
-            // do line stuff
-        }
 
-        if(last_loop && lines < min_lines){
-            // stuff with line breaks
-        }
+        free(line_fmtd);
+        lines++;
 
-        chars++;
+        // TODO: condition on wrap options 
 
-        if(last_loop)
+        // check last line
+        if(lines == max_lines && *linebreak == '\0'){
             break;
+        }
+        // check last line before finishing
+        else if(*linebreak == '\0' && lines < min_lines){
+            for(int i = lines; i < max_lines; i++)
+                strcat_realloc(&str_fmtd, "\n");
+
+            break;
+        }
+        // check overflow lines
+        else if(lines >= max_lines){
+            strfmt_t lines_fmt = fmt & strfmt_lines_bitmask; 
+
+            // just cut 
+            if(lines_fmt == strfmt_lines_cut || lines_fmt == 0){
+                break;
+            }
+            // scroll string down, removing from the beggining
+            else if(lines_fmt == strfmt_lines_scroll){
+                char *pbrk = strchr(str_fmtd, '\n');
+
+                if(pbrk == NULL)
+                    pbrk = strchr(str_fmtd, '\0');
+                else
+                    pbrk++;
+                
+                strtrimbeg_realloc(&str_fmtd, pbrk);
+                lines--;
+            }
+        }
+
+
+        if(line_break_pos == NULL)
+            line = linebreak;
+        else
+            line = (char*)(line_break_pos - 1);
     }
 
-    return base_string;
+    free(base_string);
+    return str_fmtd;
 }
 
 // better mvwprintw
@@ -315,7 +565,7 @@ void mvwprintwln(WINDOW *win, int y, int x, char *string){
             mvwprintw(win, y + cur_line, x, cur_string);
 
             cursor++;
-            run_whitespace(&cursor);
+            // run_whitespace(&cursor);
             cur_string = cursor;
 
             // so the next cursor++ on the for loop goes on top of the next letter
@@ -332,18 +582,23 @@ void mvwprintwln(WINDOW *win, int y, int x, char *string){
 }
 
 // draw text with a frame
-void wdraw_label(WINDOW *win, int y, int x, int minh, int minw, int maxh, int maxw, 
-    char *text, frame_charset_t frame, chtype fill){
+void wdraw_label(WINDOW *win, char *text, int y, int x, int minh, int maxh, 
+     int minw, int maxw, strfmt_t fmt, frame_charset_t frame, chtype fill, void *optional_data){
 
-    int lines, max_w;
-    
-    strlnmaxw(text, &lines, &max_w);
+    size_t lines, max_w;
 
+    char *text_fmt = strfmtr(text, minh - 2, maxh - 2, minw - 2, maxw - 2, fmt, optional_data);
+
+    strlnmaxw(text_fmt, &lines, &max_w);
 
     wdraw_rect(win, lines + 2, max_w + 2, y, x, frame, fill);
     
-    mvwprintwln(win, y + 1, x + 1, text);
+    mvwprintwln(win, y + 1, x + 1, text_fmt);
+
+    free(text_fmt);
 }
+
+
 
 int main(int argc, char **argv){
 
@@ -424,8 +679,16 @@ int main(int argc, char **argv){
         wborder_frame(nodes_win, frame_normal);
         mvwprintw(nodes_win, 0, 3, " Network ");
         // wdraw_rect(nodes_win, 7, nodes_win_size.w - 2, 1, 1, frame_dotted, ' ');
-        // wdraw_label(nodes_win, 1, 1, "IP: 0000\nPORT:2222", frame_solid, ' ');
-        mvwprintwln(nodes_win, 1, 1, "IP: 0000\nPORT:2222\nMASK:127");
+        wdraw_label(
+            nodes_win, "#1234\njoao-peterson\n192.168.1.255:4092", 
+            1, 1, 5, 5, 28, 28,
+            strfmt_align_center | strfmt_lines_scroll |strfmt_linebreak_no_wrap_dot_dot_dot,
+            frame_dotted, ' ',
+            NULL
+        );
+
+        // char *aligned = strfmtr("#1234\njoao-peterson\n192.168.1.255:4092", 2, 2, 19, 19, strfmt_align_center | strfmt_lines_scroll |strfmt_linebreak_no_wrap_dot_dot_dot, NULL);
+        // mvwprintwln(nodes_win, 1, 1, aligned);
 
         // input
         wclear(input_win);
